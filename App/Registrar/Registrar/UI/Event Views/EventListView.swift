595a281300eecae614e79d29dc9ba8dec69d3e5f
Zeke Abuhoff <zabuhoff@gmail.com>
2025-06-02 15:50:06 -0700

    accept or reject event invitations on event list view

--- App/Registrar/Registrar/UI/Event Views/EventListView.swift
+++ App/Registrar/Registrar/UI/Event Views/EventListView.swift
@@ -11,6 +11,7 @@ import Combine
 struct EventListView: View {
     @EnvironmentObject var flow: Flow
 
+    @State var eventInvitations: [EventInvitation] = []
     @State var events: [Event] = []
 
     @State var cancellables = Set<AnyCancellable>()
@@ -18,13 +19,28 @@ struct EventListView: View {
     var body: some View {
         NavigationStack(path: $flow.path) {
             VStack {
+                if !events.isEmpty { Text("INVITATIONS") }
+                List {
+                    ForEach(eventInvitations) { eventInvitation in
+                        HStack {
+                            VStack {
+                                Text("You're invited to \(eventInvitation.eventName)")
+                                Text("by \(eventInvitation.senderName)")
+                            }
+                            VStack {
+                                Button("Accept") { accept(invitation: eventInvitation) }
+                                Button("Reject") { reject(invitation: eventInvitation) }
+                            }
+                        }
+                    }
+                }
+
                 Text("EVENTS")
-                
                 List {
                     ForEach(events) { event in
                         NavigationLink(value: event) { Text(event.title) }
                     }
-                }.onAppear(perform: getEvents)
+                }.onAppear(perform: getData)
                     .navigationDestination(for: Event.self) { event in
                         EventDetailsView(event: event)
                     }
@@ -40,9 +56,31 @@ struct EventListView: View {
         }
     }
 
-    func getEvents() {
+    func getData() {
+        Provide.getEventInvitations()
+            .sinkValue($eventInvitations, logPrefix: "Get Event Invitations")
+            .store(in: &cancellables)
+
         Provide.getMyEvents()
             .sinkValue($events, logPrefix: "Get Events")
+            .store(in: &cancellables)
+    }
+
+    func accept(invitation: EventInvitation) {
+        Provide.acceptEventInvitation(invitation)
+            .sinkCompletion { _ in
+                log("Accepted Event Invitation: \(invitation.eventName)", level: .verbose)
+                getData()
+            }
+            .store(in: &cancellables)
+    }
+
+    func reject(invitation: EventInvitation) {
+        Provide.rejectEventInvitation(invitation)
+            .sinkCompletion { _ in
+                log("Rejected Event Invitation: \(invitation.eventName)", level: .verbose)
+                getData()
+            }
             .store(in: &cancellables)
     }
 }
